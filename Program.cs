9a2b160343ccbe085824eba5017a10cb15916b55
SebasAlvarez15 <sebastian52221@gmail.com>
2022-08-21 13:37:24 -0500

    Ejercicio2

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Bienvenidos a la Sala de Juegos" 
++"\nPor favor ingrese su nombre completo : ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su Edad : ");
+uint edad = uint.Parse(Console.ReadLine());
+
+if(edad < 4){
+    Console.WriteLine("Nombre: "+ nombre + "\nEdad: "+edad +"\nPuedes entrar GRATIS");
+    }
+if (edad > 4 & edad < 18){
+     int valor = 15000;
+     int desc = 15000-750;
+      Console.WriteLine("Nombre: "+ nombre + "\nEdad: "+edad 
+      + "\nDebes pagar $15000, Por ser menor de edad " + "\nTienes un descuento del 5%: "+ valor*0.05
+      +"\nTotal a pagar: " + desc );
+    }
+
+    if (edad > 18){
+     int valor = 15000;
+     int desc = 15000-450;
+      Console.WriteLine("Nombre: "+ nombre + "\nEdad: "+edad 
+      + "\nDebes pagar $15000, Por ser mayor de edad " + "\nTienes un descuento del 3%: "+ valor*0.03
+      +"\nTotal a pagar: " + desc );
+    }
+
+
+
+//Console.WriteLine(nombre);
